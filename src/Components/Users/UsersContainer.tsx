8dd5ff4c0a3a80f183cf5ada607150d0ce98082d
ArtemSorokin <artemsorokin4395@gmail.com>
2022-11-13 15:19:41 +0300

    1. mapDisoatchToProps. Вместо передачи фий передаем объект с названием свойств = экшн криеторам

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -23,12 +23,12 @@ export type mapStateToPropsType = {
     isFetchingPreloadGif: boolean
 }
 export type mapDispatchToPropsType = {
-    follow: (userId: number) => void,
-    unFollow: (userId: number) => void,
-    setUsers: (usersArray: Array<UserUnitType>) => void
-    setCurrentPage:(pageNumber:number)=> void
-    settotalCount:(totalCount: number) => void
-    setIsFetchingPreloadGif: (isFetchingPreloadGif:boolean)=> void
+    FollowUserActionCreator: (userId: number) => void
+        UnfollowUserActionCreator: (userId: number) => void,
+    SetUsersActionCreator: (usersArray: Array<UserUnitType>) => void
+    SetCurrentPageActionCreator:(pageNumber:number)=> void
+    settotalCountPageActionCreator:(totalCount: number) => void
+    setIsFetchingPreloadGifAC: (isFetchingPreloadGif:boolean)=> void
 }
 export type UserComponentPropsType = mapStateToPropsType & mapDispatchToPropsType
 
@@ -37,20 +37,20 @@ export class UsersClassComponent extends React.Component<UserComponentPropsType>
 
     componentDidMount() {
         // запрос списка юзеров
-        this.props.setIsFetchingPreloadGif(true)
+        this.props.setIsFetchingPreloadGifAC(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pagesize}`).then(response => {
-            this.props.setIsFetchingPreloadGif(false)
-            this.props.setUsers(response.data.items)
-            this.props.settotalCount(response.data.totalCount)
+            this.props.setIsFetchingPreloadGifAC(false)
+            this.props.SetUsersActionCreator(response.data.items)
+            this.props.settotalCountPageActionCreator(response.data.totalCount)
         })
     }
 
     setCurrentPage = (currentPage:number)=>{
-        this.props.setCurrentPage(currentPage)
-        this.props.setIsFetchingPreloadGif(true)
+        this.props.SetCurrentPageActionCreator(currentPage)
+        this.props.setIsFetchingPreloadGifAC(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pagesize}`).then(response => {
-            this.props.setIsFetchingPreloadGif(false)
-            this.props.setUsers(response.data.items)
+            this.props.setIsFetchingPreloadGifAC(false)
+            this.props.SetUsersActionCreator(response.data.items)
         })
     }
 
@@ -64,8 +64,8 @@ export class UsersClassComponent extends React.Component<UserComponentPropsType>
             totalCount={this.props.totalCount}
             currentPage={this.props.currentPage}
             pagesize={this.props.pagesize}
-            follow={this.props.follow}
-            unFollow={this.props.unFollow}
+            follow={this.props.FollowUserActionCreator}
+            unFollow={this.props.UnfollowUserActionCreator}
             setCurrentPage={this.setCurrentPage} />
         </>
 
@@ -83,29 +83,35 @@ const mapStateToProps = (state: RootStateType): mapStateToPropsType => {
     }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
+// const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
+//
+//     return {
+//         follow: (userId: number) => {
+//             dispatch(FollowUserActionCreator(userId))
+//         },
+//         unFollow: (userId: number) => {
+//             dispatch(UnfollowUserActionCreator(userId))
+//         },
+//         setUsers: (usersArray: Array<UserUnitType>) => {
+//             dispatch(SetUsersActionCreator(usersArray))
+//         },
+//         setCurrentPage: (currentPage:number) => {
+//             dispatch(SetCurrentPageActionCreator(currentPage))
+//         },
+//         settotalCount: (totalCount:number) => {
+//             dispatch(settotalCountPageActionCreator(totalCount))
+//         },
+//         setIsFetchingPreloadGif: (isFetchingPreloadGif:boolean)=> {
+//             dispatch(setIsFetchingPreloadGifAC(isFetchingPreloadGif))
+//         }
+//     }
+// }
 
-    return {
-        follow: (userId: number) => {
-            dispatch(FollowUserActionCreator(userId))
-        },
-        unFollow: (userId: number) => {
-            dispatch(UnfollowUserActionCreator(userId))
-        },
-        setUsers: (usersArray: Array<UserUnitType>) => {
-            dispatch(SetUsersActionCreator(usersArray))
-        },
-        setCurrentPage: (currentPage:number) => {
-            dispatch(SetCurrentPageActionCreator(currentPage))
-        },
-        settotalCount: (totalCount:number) => {
-            dispatch(settotalCountPageActionCreator(totalCount))
-        },
-        setIsFetchingPreloadGif: (isFetchingPreloadGif:boolean)=> {
-            dispatch(setIsFetchingPreloadGifAC(isFetchingPreloadGif))
-        }
-    }
-}
 
-
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersClassComponent)
+export const UsersContainer = connect(mapStateToProps, {
+    FollowUserActionCreator,
+    UnfollowUserActionCreator,
+    SetUsersActionCreator,
+    SetCurrentPageActionCreator,
+    settotalCountPageActionCreator,
+    setIsFetchingPreloadGifAC   })(UsersClassComponent)
